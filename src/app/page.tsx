ef634aa6d4dca09ef1d2261bdd0bc8161d7e33b0
Sean <seankel@comcast.net>
2023-10-07 23:41:27 -0700

    Added fake first page that calls csport to run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  redirect('/about');
+
+  useEffect(() => {
+    console.log('fetch for csport ran');
+    fetch('https://csport.azurewebsites.net').then().catch();
+  }, []);
+
+
+  useEffect(() => {
+    console.log('fetch for sportnode ran');
+    fetch('https://sportnode.azurewebsites.net/ConnectionStatus').then().catch();
+  }, []);
+
+  useEffect(() => {
+    redirect('/about');
+  }, []); 
 }
